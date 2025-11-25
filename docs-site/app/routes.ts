8da18d88a1124b053855b99b190804dcd8563732
Maxim Shelepov <mimaishel@ibm.com>
2025-11-25 12:38:04 -0600

    chore: syncing with the latest ibm cloud docs developments.

--- docs-site/app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-import { flatRoutes } from "./util/routes";
-
-export default [
-  ...flatRoutes
-  // ...Object.keys(routesPages).map((routePath) => route(routePath, routesPages[routePath]))
-  // route("agents/base-agent/", "routes/base-agent.tsx"),
-  // route("agents/account-admin-agent", "routes/ibm-cloud-account-admin-agent.tsx"),
-] satisfies RouteConfig;
